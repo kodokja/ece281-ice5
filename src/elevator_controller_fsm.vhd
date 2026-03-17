f91b2ee5e61cad7dd36447d1f7f4dd3a97753273
Rodriguez <c28david.rodriguez@afacademy.af.edu>
2026-03-16 23:39:01 -0600

    elevator waveform added and testbench completed

--- src/elevator_controller_fsm.vhd
+++ src/elevator_controller_fsm.vhd
@@ -95,8 +95,21 @@ begin
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	
 	-- Next State Logic
+	f_Q_next <= s_floor2 when (i_up_down = '1' and i_stop = '0' and f_Q = s_floor1) else -- going up
+                s_floor3 when (i_up_down = '1' and i_stop = '0' and f_Q = s_floor2) else
+                s_floor4 when (i_up_down = '1' and i_stop = '0' and f_Q = s_floor3) else
+                s_floor3 when (i_up_down = '0' and i_stop = '0' and f_Q = s_floor4) else -- going down
+                s_floor2 when (i_up_down = '0' and i_stop = '0' and f_Q = s_floor3) else
+                s_floor1 when (i_up_down = '0' and i_stop = '0' and f_Q = s_floor2) else
+            s_floor2; -- default case
   
 	-- Output logic
+	with f_Q select
+        o_floor <= "0001" when s_floor1,
+                   "0010" when s_floor2,
+                   "0011" when s_floor3,
+                   "0100" when s_floor4,
+            "0001" when others; -- default is floor1
 
 	-------------------------------------------------------------------------------------------------------
 	
@@ -104,7 +117,18 @@ begin
 	
 	-- State register ------------
 	
-	
+	register_proc : process(i_clk, i_reset, i_stop)
+	begin
+	   if(rising_edge(i_clk)) then
+	       if i_reset = '1' then
+	       f_Q <= s_floor2;
+	       elsif i_stop = '1' then
+	       f_Q <= f_Q;
+	       else
+	       f_Q <= f_Q_next;
+	       end if;
+	   end if;
+	end process register_proc;
 	-------------------------------------------------------------------------------------------------------
 	
 	
--- src/elevator_controller_fsm_tb.vhd
+++ src/elevator_controller_fsm_tb.vhd
@@ -103,28 +103,33 @@ begin
         w_reset <= '1';  wait for k_clk_period;
             assert w_floor = x"2" report "bad reset" severity failure; 
         -- clear reset
-		
+		w_reset <= '0';
 		-- active UP signal
 		w_up_down <= '1'; 
 		
 		-- go up a floor
-        w_stop <= '0';  wait for k_clk_period;
+        w_stop <= '0';  wait for k_clk_period*1;
             assert w_floor = x"3" report "bad up from floor2" severity failure;
 		-- try waiting on a floor
-        w_stop <= '1';  wait for k_clk_period * 2;
+        w_stop <= '1';  wait for k_clk_period*1;
             assert w_floor = x"3" report "bad wait on floor3" severity failure;
 		--  go up again
-		
+		w_stop <= '0'; wait for k_clk_period;
+		    assert w_floor = x"4" report "bad up from floor3" severity failure;
 		-- go back down one floor
-		
+		w_up_down <= '0'; wait for k_clk_period;
+		    assert w_floor = x"3" report "bad down from floor4" severity failure;
 		-- go up the rest of the way
-		
+		w_up_down <= '1'; wait for k_clk_period;
+		    assert w_floor = x"4" report "bad up from floor3" severity failure;
 		-- stop at top
-        
+        w_stop <= '1'; wait for k_clk_period;
+            assert w_floor = x"4" report "bad wait on floor4";
         -- go all the way down DOWN (how many clock cycles should that take?)
-        w_up_down <= '0'; 
-  
-		  	
+        w_up_down <= '0'; wait for k_clk_period;
+        wait for k_clk_period;
+        wait for k_clk_period;
+            assert w_floor = x"1" report "bad all the way down"; 
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
